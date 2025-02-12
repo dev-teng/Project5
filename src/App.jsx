962af7afea67761689edb60bc0b290787fcd9d7d
dev-teng <vnctlcrn@gmail.com>
2025-02-12 14:40:17 +0800

    added login section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import Login from './Login';
 function App() {
 
 
   return (
 
-      <h1>Hello world</h1>
+     <>
+     <Login />
+     </>
   )
 }
 
---
+++ src/Login.jsx
@@ -0,0 +1,18 @@
+function Login () {
+  return(
+    <div className="container-fluid p-5" style={{width: "50rem"}}>
+      <div className="bg-danger-subtle p-5 m-5 rounded">
+        <h1 className="text-center">Welcome to Soul+Sync</h1>
+        <p className="text-center">Your perfect match is just a click away 💓.</p>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" className="form-control"></input>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="email" className="form-control"></input>
+        <button className="btn btn-dark mt-3">Login</button>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Login
