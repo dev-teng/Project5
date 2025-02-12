80fa20987b56d556cb374fcdd718bc9584c71e10
dev-teng <vnctlcrn@gmail.com>
2025-02-12 13:52:40 +0800

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+function App() {
+
+
+  return (
+
+      <h1>Hello world</h1>
+  )
+}
+
+export default App
