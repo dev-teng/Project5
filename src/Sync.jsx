17169117648cb159565c78a77826395722307ede
dev-teng <vnctlcrn@gmail.com>
2025-02-17 08:25:51 +0800

    change the edit and delete button

--- src/Sync.jsx
+++ src/Sync.jsx
@@ -2,21 +2,15 @@ function Sync ({email , displayName, date_posted, body, onDelete,}) {
   return ( 
     <div className="col-md-8 offset-md-2 border p-3 bg-danger-subtle mt-3">
         <div className="card-body">
-          <div className="d-flex justify-content-end">
-            <div className="dropdown">
-                <button className="btn btn-danger-subtle dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                </button>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Edith</a></li>
-                  <li><a className="dropdown-item" href="#" onClick={onDelete}>Delete</a></li>
-                </ul>
-            </div>
-
-          </div>
           <span className="fs-4 fw-bold me-2 lh-1">{displayName}</span>
             <br/>
           <small className="text-secondary lh-1">{date_posted}</small>
             <p className="mt-3 border bg-white p-3 rounded">{body}</p>
+            <div className="d-flex justify-content-end">
+            <button className="btn btn-outline-success me-2">Edit</button>
+            <button onClick={onDelete} className="btn btn-outline-danger">Delete</button>
+            </div>
+            
         </div>
     </div>
   )
